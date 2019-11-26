d0da06099bef744341f8ebb658bd2e87395baa3c
ian <ianxm@jhu.edu>
2019-11-26 06:57:43 -0500

    refactor so that 'read-metrics' is reuseable

--- tracker.el
+++ tracker.el
@@ -30,111 +30,97 @@
 
 ;;; Code:
 
-(defun tracker--min-date (d1 d2)
-  "Returns the earlier of the given dates D1 and D2."
-  (if (time-less-p d1 d2) d1 d2))
+(defmacro tracker--min-date (d1 d2)
+  "Return the earlier of the given dates D1 and D2."
+  `(if (time-less-p ,d1 ,d2) ,d1 ,d2))
 
-(defun tracker--max-date (d1 d2)
-  "Returns the later of the given dates D1 and D2."
-  (if (time-less-p d1 d2) d2 d1))
+(defmacro tracker--max-date (d1 d2)
+  "Return the later of the given dates D1 and D2."
+  `(if (time-less-p ,d1 ,d2) ,d2 ,d1))
 
+(defmacro tracker--today ()
+  "Get a time value for today."
+  `(let ((now (mapcar #'string-to-number
+                      (split-string (format-time-string "%F") "-"))))
+     (encode-time 0 0 0
+                  (nth 2 now)
+                  (nth 1 now)
+                  (nth 0 now))))
 
-;; todo can I clear this variable when the output buffer is killed?
 (defvar tracker-metric-index nil
   "This is the list of metrics read from the diary file.
-It is a list containing: (name count first last) for each
-metric.")
+It is a list containing: (name count first last) for each metric.
+It is cleared when the tracker output buffer is killed, forcing
+the diary file to be re-read if the data is needed again.")
 
-(defun tracker--read-metrics (&optional force)
-  "This reads the diary file and fills in `tracker-metric-index'.
-This only run if `tracker-metric-index' is empty or `force' is non-nil."
-  (when (or force
-            (not tracker-metric-index))
-    (let (metrics metric-name date-fields metric-date existing-metric metric
-                  (metric-symbols (make-vector 5 0)))
-      (with-temp-buffer
-        (insert-file-contents diary-file)
-        (dolist (line (split-string (buffer-string) "\n" t))
-          ;; build the list of metrics
-          (when (string-match "\\([[:digit:]\-]+\\) \\([[:ascii:]]+\\) [[:digit:]\.]+" line) ; valid diary entry
-            (setq metric-name (match-string 2 line))
-            (setq date-fields (mapcar #'string-to-number (split-string (match-string 1 line) "-")))
-            (setq metric-date (encode-time 0 0 0
-                                           (nth 2 date-fields)   ; day
-                                           (nth 1 date-fields)   ; month
-                                           (nth 0 date-fields))) ; year
-            (setq existing-metric (plist-get metrics (intern metric-name metric-symbols)))
-            (if (not existing-metric)
-                (setq metrics (plist-put metrics
-                                         (intern metric-name metric-symbols)
-                                         (list metric-name 1 metric-date metric-date)))
-              (setcar (nthcdr 1 existing-metric) (1+ (nth 1 existing-metric)))
-              (setcar (nthcdr 2 existing-metric) (tracker--min-date (nth 2 existing-metric) metric-date))
-              (setcar (nthcdr 3 existing-metric) (tracker--max-date (nth 3 existing-metric) metric-date))))))
+(defun tracker--process-diary (filter action)
+  "Read the diary file.
+For each valid metrics entry found, apply the given FILTER and
+ACTION."
+  (let (metric-name date-fields metric-date metric-value)
+    (with-temp-buffer
+      (insert-file-contents diary-file)
+      (dolist (line (split-string (buffer-string) "\n" t))
+        (when (string-match "\\([[:digit:]\-]+\\) \\([[:ascii:]]+\\) \\([[:digit:]\.]+\\)" line) ; valid diary entry
+          (setq metric-name (match-string 2 line)
+                metric-value (string-to-number (match-string 3 line))
+                date-fields (mapcar #'string-to-number ; oddly, this messes up match-data so it must be done last
+                                    (split-string (match-string 1 line) "-"))
+                metric-date (encode-time 0 0 0
+                                         (nth 2 date-fields)   ; day
+                                         (nth 1 date-fields)   ; month
+                                         (nth 0 date-fields)))  ; year
+          (if (funcall filter metric-date metric-name)
+              (funcall action metric-date metric-name metric-value)))))))
 
-      ;; now `metrics' is a plist like "name -> (name count first last)"
-      ;; get the property values from the plist
-      (setq metric metrics)
-      (while (cdr metric)
-        (setq metric (cdr metric))
-        (setq tracker-metric-index (nconc tracker-metric-index (list (car metric))))
-        (setq metric (cdr metric)))
+(defun tracker-clear-data ()
+  "Clear the data saved in `tracker-metric-index'.
+By clearing it now we force it to be re-read from the diary file
+the next time it is needed."
+  (when (string= (buffer-name (current-buffer)) "*Tracker Output*")
+    (setq tracker-metric-index nil)
+    (remove-hook 'kill-buffer-hook #'tracker-clear-data)))
+
+(defun tracker--load-index ()
+  "Make sure the metric index has been populated.
+This reads the diary file and fills in `tracker-metric-list' if
+it is nil."
+  (when (not tracker-metric-index)
+    (let (metrics
+          existing-metric
+          (metric-symbols (make-vector 5 0)))
+      (defun tracker--list-action (date name _value)
+        (setq existing-metric (plist-get metrics (intern name metric-symbols)))
+        (if (not existing-metric)
+            (setq metrics (plist-put metrics
+                                     (intern name metric-symbols)
+                                     (list name 1 date date)))
+          (setcar (nthcdr 1 existing-metric) (1+ (nth 1 existing-metric)))
+          (setcar (nthcdr 2 existing-metric) (tracker--min-date (nth 2 existing-metric) date))
+          (setcar (nthcdr 3 existing-metric) (tracker--max-date (nth 3 existing-metric) date))))
+
+      ;; read the diary file, fill `metrics' plist with "name -> (name count first last)"
+      (tracker--process-diary (lambda (_date _name) t) ; don't filter out any valid entries
+                              'tracker--list-action)
+
+      ;; get the property values from the `metrics' plist
+      (let ((metric-iter metrics))
+        (while (cdr metric-iter)
+          (setq metric-iter (cdr metric-iter)
+                tracker-metric-index (nconc tracker-metric-index (list (car metric-iter)))
+                metric-iter (cdr metric-iter))))
 
       ;; sort by last update date
       (setq tracker-metric-index (sort tracker-metric-index
-                                       (lambda (a b) (time-less-p (nth 3 b) (nth 3 a))))))))
-
-(defun tracker-clear ()
-  "Clear the data saved in `tracker-metric-index' so it must be
-re-read from the diary file."
-  (interactive)
-  (setq tracker-metric-index nil))
-
-(defun tracker--read-metrics-slow ()
-  "This is an attempt o read metrics from the diary file using
-`diary-list-entries', but is too slow to use. (~50 days processed
-per second)"
-  (let ((entries (diary-list-entries '(01 01 2015) 100 t))
-        (metric-symbols (make-vector 5 0))
-        metrics entry-body metric-name metric-date existing-metric count first last
-        (time-start (float-time))
-        time-end)
-    (dolist (metric entries)
-      ;; build the list of metrics
-      (setq entry-body (nth 1 metric))
-      (when (string-match "\\([[:word:]]+\\) [[:ascii:]\.]+" entry-body) ; valid diary entry
-        (setq metric-name (match-string 1 entry-body))
-        (setq metric-date (encode-time 0 0 0
-                                       (nth 1 (nth 0 metric))
-                                       (nth 0 (nth 0 metric))
-                                       (nth 2 (nth 0 metric))))
-        (setq existing-metric (plist-get metrics (intern metric-name metric-symbols)))
-        (if (not existing-metric)
-            (setq metrics (plist-put metrics
-                                     (intern metric-name metric-symbols)
-                                     (list metric-name 1 metric-date metric-date)))
-          (setcar (nthcdr 1 existing-metric) (1+ (nth 1 existing-metric)))
-          (setcar (nthcdr 2 existing-metric) (tracker--min-date (nth 2 existing-metric) metric-date))
-          (setcar (nthcdr 3 existing-metric) (tracker--max-date (nth 3 existing-metric) metric-date)))))
-    (message "time %s" (- (float-time) time-start))
-
-    ;; get the property values from the plist
-    (setq metric metrics)
-    (while (cdr metric)
-      (setq metric (cdr metric))
-      (setq tracker-metric-index (nconc tracker-metric-index (list (car metric))))
-      (setq metric (cdr metric)))
-
-    ;; sort by recency
-    (setq tracker-metric-index (sort tracker-metric-index
-                                     (lambda (a b) (time-less-p (nth 3 b) (nth 3 a)))))))
+                                       (lambda (a b) (time-less-p (nth 3 b) (nth 3 a)))))
+      (add-hook 'kill-buffer-hook #'tracker-clear-data))))
 
 (defun tracker-list ()
   "Display a list of all saved metrics in the output buffer.
 This reads the diary file."
   (interactive)
 
-  (tracker--read-metrics)               ; read the diary file
+  (tracker--load-index)
 
   (let ((buffer (get-buffer-create "*Tracker Output*")))
     (set-buffer buffer)
@@ -170,17 +156,29 @@ This reads the diary file."
   "Get a tabular view of the requested metric."
   (interactive)
 
-  ;; make sure we've read the diary file so we know the existing metric names
-  (tracker--read-metrics)
-  (setq metric-names (mapcar (lambda (metric) (nth 0 metric)) tracker-metric-index))
+  ;; make sure `tracker-metric-index' has been populated
+  (tracker--load-index)
 
-  ;; ask for params
-  (setq metric (completing-read "Metric: " metric-names nil t))
-  (setq date-grouping (completing-read "Group dates by: " tracker-date-grouping-options nil t nil nil "month"))
-  (setq value-transform (completing-read "Value transform: " tracker-value-transform-options nil t nil nil "total"))
-  (message "%s %s %s" date-grouping value-transform metric)
+  (let (all-metric-names
+        metric-name date-grouping value-transform
+        range-start range-end)
+    (setq all-metric-names (mapcar (lambda (metric) (nth 0 metric)) tracker-metric-index))
 
-  ;; range may have been passed in from cal, else set it to full
+    ;; ask for params
+    (setq metric-name (completing-read "Metric: " all-metric-names nil t)
+          date-grouping (completing-read "Group dates by: " tracker-date-grouping-options nil t nil nil "month")
+          value-transform (completing-read "Value transform: " tracker-value-transform-options nil t nil nil "total"))
+    (message "params %s %s %s" date-grouping value-transform metric-name)
+
+    ;; todo try getting range from the calendar
+    (setq range-start nil
+          range-end (tracker--today))
+    (message "range %s %s" range-start range-end)
+
+    ;; read diary file, filtering for the entries we're interested in
+
+    ;; add filtered entries to the table data structure
+    )
 
   )
 
