df33246e1c6e3ded99ff1193fc719edc97074c6b
ian <ianxm@jhu.edu>
2019-11-30 12:48:44 -0500

    implemented table report

--- tracker.el
+++ tracker.el
@@ -6,7 +6,7 @@
 ;; URL: http://github.com/ianxm/emacs-tracker
 ;; Version: 0.0.1
 ;; Keywords: docs
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "24.4") (seq "2.20"))
 
 ;; This file is not part of GNU Emacs.
 
@@ -29,6 +29,8 @@
 ;; from the personal metrics data found in your diary entries.
 
 ;;; Code:
+
+(require 'seq)
 
 (defmacro tracker--min-date (d1 d2)
   "Return the earlier of the given dates D1 and D2."
@@ -104,7 +106,7 @@ it is nil."
       (let ((metric-iter metrics))
         (while (cdr metric-iter)
           (setq metric-iter (cdr metric-iter)
-                tracker-metric-index (nconc tracker-metric-index (list (car metric-iter)))
+                tracker-metric-index (cons (car metric-iter) tracker-metric-index)
                 metric-iter (cdr metric-iter))))
 
       ;; sort by last update date
@@ -149,6 +151,86 @@ This reads the diary file."
 (defvar tracker-value-transform-options
   '(total count percent per-day per-week per-month per-year))
 
+
+(defun tracker--date-to-bin (date date-grouping)
+  "Return the date at the start of the DATE-GROUPING containing DATE."
+  (if (eq date-grouping 'full)
+      'full
+    (let ((date-fields (decode-time date))
+          (offset (cond
+                   ((eq date-grouping 'day) 0)
+                   ((eq date-grouping 'week) (nth 6 (decode-time date)))
+                   ((eq date-grouping 'month) (1- (nth 3 (decode-time date))))
+                   ((eq date-grouping 'year) (1- (string-to-number (format-time-string "%j" date)))))))
+      (encode-time 0 0 0
+                   (- (nth 3 date-fields) offset)
+                   (nth 4 date-fields)
+                   (nth 5 date-fields)))))
+
+(defun tracker--date-to-next-bin (date date-grouping)
+  "Return the date at the start of the DATE-GROUPING following the one that starts with DATE."
+  (if (eq date-grouping 'full)
+      'full
+    (let* ((date-fields (decode-time date))
+           (is-dst (nth 7 date-fields))
+           (next-date-fields date-fields)
+           next-date)
+      (cond
+       ((eq date-grouping 'day) (setcar (nthcdr 3 next-date-fields) (1+ (nth 3 next-date-fields))))
+       ((eq date-grouping 'week) (setcar (nthcdr 3 next-date-fields) (+ 7 (nth 3 next-date-fields))))
+       ((eq date-grouping 'month) (setcar (nthcdr 4 next-date-fields) (1+ (nth 4 next-date-fields))))
+       ((eq date-grouping 'year) (setcar (nthcdr 5 next-date-fields) (1+ (nth 5 next-date-fields)))))
+      (setq next-date (apply 'encode-time next-date-fields))
+      (setq next-date-fields (decode-time next-date))
+      ;; suppress daylight savings shifts
+      (when (and (not is-dst)
+               (nth 7 next-date-fields))
+        (setq next-date (seq-take (time-subtract next-date (seconds-to-time 3600)) 2)))
+      (when (and is-dst
+                 (not (nth 7 next-date-fields)))
+        (setq next-date (seq-take (time-add next-date (seconds-to-time 3600)) 2)))
+      ;; return next-date
+      next-date)))
+
+(defun tracker--val-to-bin (value value-transform date-grouping)
+  ""
+  (cond
+   ((eq value-transform 'count) 1)
+   ((eq value-transform 'percent) 1)
+   (t value)))
+
+(defun tracker--format-bin (date date-grouping)
+  "Format the bin start DATE to the bin name used in reporting.
+
+Using the DATE-GROUPING we can eliminate redundant info from bin
+names."
+  (cond
+       ((eq date-grouping 'day) (format-time-string "%F" date))
+       ((eq date-grouping 'week) (format-time-string "%F" date))
+       ((eq date-grouping 'month) (format-time-string "%Y-%m" date))
+       ((eq date-grouping 'year) (format-time-string "%Y" date))))
+
+
+(defun tracker--bin-to-val (value value-transform date-grouping full-span)
+  "Transform and format the bin VALUE into the value used in reporting.
+
+The VALUE-TRANSFORM and DATE-GROUPING are needed to determine how
+to transform the value."
+  (let ((bin-duration (cond
+                       ((eq date-grouping 'day) 1.0)
+                       ((eq date-grouping 'week) 7.0)
+                       ((eq date-grouping 'month) 30.0)
+                       ((eq date-grouping 'year) 365.0)
+                       ((eq date-grouping 'full) (float full-span)))))
+    (cond
+     ((eq value-transform 'total) value)
+     ((eq value-transform 'count) value)
+     ((eq value-transform 'percent) (format "%.1f" (* (/ value bin-duration) 100)))
+     ((eq value-transform 'per-day) (format "%.1f" (* value (/ 1 bin-duration))))
+     ((eq value-transform 'per-week) (format "%.1f" (* value (/ 7 bin-duration))))
+     ((eq value-transform 'per-month) (format "%.1f" (* value (/ 30 bin-duration))))
+     ((eq value-transform 'per-year) (format "%.1f" (* value (/ 365 bin-duration)))))))
+
 (defun tracker-table ()
   "Get a tabular view of the requested metric."
   (interactive)
@@ -156,22 +238,83 @@ This reads the diary file."
   ;; make sure `tracker-metric-index' has been populated
   (tracker--load-index)
 
-
   (let ((all-metric-names (mapcar (lambda (metric) (nth 0 metric)) tracker-metric-index))
-        metric-name date-grouping value-transform)
+        (bin-data (make-hash-table :test 'equal))
+        metric-name date-grouping value-transform
+        sorted-bin-data)
+
     ;; ask for params
     (setq metric-name (intern (completing-read "Metric: " all-metric-names nil t) tracker-metric-names)
-          date-grouping (completing-read "Group dates by: " tracker-date-grouping-options nil t nil nil "month")
-          value-transform (completing-read "Value transform: " tracker-value-transform-options nil t nil nil "total"))
-    (message "params: %s %s %s" date-grouping value-transform metric-name)
+          date-grouping (intern (completing-read "Group dates by: " tracker-date-grouping-options nil t nil nil "month"))
+          value-transform (intern (completing-read "Value transform: " tracker-value-transform-options nil t nil nil "total")))
+    ;; (message "params: %s %s %s" date-grouping value-transform metric-name)
 
-    ;; read diary file, filtering for the entries we're interested in
-    (defun tracker--table-filter (_date name)
-      (eq name metric-name))
-    (defun tracker--table-action (date name value)
-      (message "%s %s %s" date name value))
-    (tracker--process-diary 'tracker--table-filter 'tracker--table-action)))
+    (let* ((first-date (nth 2 (nth 0 (seq-filter (lambda (item) (eq (car item) metric-name)) tracker-metric-index))))
+           (first-date-bin (tracker--date-to-bin first-date date-grouping))
+           (today (tracker--string-to-date (format-time-string "%F")))
+           (today-bin (tracker--date-to-bin today date-grouping))
+           (full-span (and (eq date-grouping 'full)
+                           (- (time-to-days today) (time-to-days first-date))))
+           date-bin found-value)
 
+      ;; read diary file, filtering for the entries we're interested in
+      (defun tracker--table-filter (_date name)
+        (eq name metric-name))
+      (defun tracker--table-action (date name value)
+        (setq date-bin (tracker--date-to-bin date date-grouping)
+              found-value (gethash date-bin bin-data))
+        (if found-value
+            (puthash date-bin (+ found-value (tracker--val-to-bin value value-transform date-grouping)) bin-data)
+          (puthash date-bin (tracker--val-to-bin value value-transform date-grouping) bin-data)))
+
+      (tracker--process-diary 'tracker--table-filter 'tracker--table-action)
+
+      ;; fill gaps
+      (unless (eq date-grouping 'full)
+        (let ((current-date-bin first-date-bin))
+          (while (time-less-p current-date-bin today-bin)
+            (setq current-date-bin (tracker--date-to-next-bin current-date-bin date-grouping)) ; increment to next bin
+            (unless (gethash current-date-bin bin-data)
+              (puthash current-date-bin 0 bin-data)))))
+
+      ;; convert to alist and sort
+      (maphash (lambda (key value) (setq sorted-bin-data (cons (cons key value) sorted-bin-data))) bin-data)
+      (setq sorted-bin-data (sort sorted-bin-data (lambda (a b) (time-less-p (car a) (car b)))))
+
+      ;; scale and print
+      (if (eq date-grouping 'full)
+          (message "Overall %s %s: %s"
+                   metric-name
+                   (replace-regexp-in-string "-" " " (symbol-name value-transform))
+                   (tracker--bin-to-val (cdar sorted-bin-data) value-transform date-grouping full-span))
+        (let ((buffer (get-buffer-create "*Tracker Output*")))
+          (set-buffer buffer)
+          (read-only-mode -1)
+          (erase-buffer)
+
+          ;; write the metric name
+          (insert (format "%s %s by %s\n\n"
+                          metric-name
+                          (replace-regexp-in-string "-" " " (symbol-name value-transform))
+                          date-grouping))
+
+          ;; write the table
+          (insert (format "| %s | value |\n" date-grouping))
+          (insert "|--\n")
+          (dolist (bin sorted-bin-data)
+            (insert (format "| %s | %s |\n"
+                            (tracker--format-bin (car bin) date-grouping)
+                            (tracker--bin-to-val (cdr bin) value-transform date-grouping full-span))))
+
+          (goto-char (point-min))
+          (forward-line 2)
+          (orgtbl-mode t)
+          (org-ctrl-c-ctrl-c)
+
+          ;; show output buffer
+          (read-only-mode 1)
+          (set-window-buffer (selected-window) buffer))
+        ))))
 
 (provide 'tracker)
 
